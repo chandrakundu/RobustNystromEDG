05f653f54cf888e5c38333c829062dfcb217a56f
chandra.kundu <cskundu21@gmail.com>
2024-11-07 18:15:55 -0500

    added different RPCA methods

---
+++ play.m
@@ -0,0 +1,16 @@
+% create a random matrix of size m x r
+
+n = 1000;
+r = 3;
+P = rand(n,r);
+
+% create gram matrix
+D = P*P';
+
+% create 4 block matrices. top-left: 1 to n and top-right m-n to m
+% second is bottom-left 
+
+m = 30;
+E = D(1:m,1:m);
+F = D(1:m,m+1:end);
+G = D(m+1:end,m+1:end);
